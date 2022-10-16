24c6a1c577271f6994cec44b39db4f1e6da2b359
morkonda <mgsrivathsan@gmail.com>
2022-10-16 19:17:52 -0400

    Move contextmenu code

---
+++ src/js/contextmenu.js
@@ -0,0 +1,36 @@
+"use strict";
+
+chrome.runtime.onMessage.addListener(
+    function(request, sender) {
+        if (request.msg === "SHOW_PERMISSIONS") {
+            showIdPResult();
+        }
+    }
+);
+
+function showIdPResult() {
+    const card = newElement("card");
+    const p = document.createElement("p");
+    //TODO add a header indicating this is from SPEYE tool (same cue in right-click option icon)
+    p.textContent = "If you continue login, the following will be requested.";
+    card.appendChild(p);
+
+    // add a title to the card
+    const url = window.location.href;
+    const title = newElement("card-header", getIdPName(url));
+    card.appendChild(title);
+
+    // main content of the card
+    const content = newElement("card-content");
+    addContentHeader(content); // header to indicate the RP name
+    addContent(url, content); // show list of permissions
+    addOptoutNote(content); // footer note about permission opt-outs
+    card.appendChild(content);
+
+    const column = newElement("column");
+    column.appendChild(card);
+
+    column.style.position = "relative";
+    document.body.appendChild(column);
+}
+
