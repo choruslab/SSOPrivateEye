9715259d5bbfa1f7f99e02a4b578b884b587ed39
morkonda <mgsrivathsan@gmail.com>
2022-10-17 08:23:46 -0400

    Stick permissions card to IdP page

--- src/js/contextmenu.js
+++ src/js/contextmenu.js
@@ -9,11 +9,17 @@ chrome.runtime.onMessage.addListener(
 );
 
 function showIdPResult() {
+
+    // overlay mask in the background
+    const overlay = newElement("overlay");
+    overlay.style.display = "block";
+    document.body.appendChild(overlay);
+
     const card = newElement("card");
-    const p = document.createElement("p");
-    //TODO add a header indicating this is from SPEYE tool (same cue in right-click option icon)
-    p.textContent = "If you continue login, the following will be requested.";
-    card.appendChild(p);
+    // const p = document.createElement("p");
+    // //TODO add a header indicating this is from SPEYE tool (same cue in right-click option icon)
+    // p.textContent = "If you continue login, the following will be requested.";
+    // card.appendChild(p);
 
     // add a title to the card
     const url = window.location.href;
@@ -26,10 +32,29 @@ function showIdPResult() {
     addContent(url, content); // show list of permissions
     card.appendChild(content);
 
-    const column = newElement("column");
-    column.appendChild(card);
+    // add close option
+    const close = newElement("close-x", "x");
+    const cardWithClose = newElement("full-card");
 
-    column.style.position = "relative";
+    cardWithClose.appendChild(close);
+    cardWithClose.appendChild(card);
+
+    const column = newElement("column");
+    column.appendChild(cardWithClose);
+
+    column.style.position = "sticky";
+    column.style.bottom = "5px";
+    column.style.zIndex = "10";
     document.body.appendChild(column);
+
+    // listeners for close event
+    close.onclick = function() {
+        overlay.style.display = "none";
+        column.style.display = "none";
+    }
+    overlay.onclick = function() {
+        overlay.style.display = "none";
+        column.style.display = "none";
+    }
 }
 
